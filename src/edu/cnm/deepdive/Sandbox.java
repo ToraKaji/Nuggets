954d57671a634dd62030e120710e1dbe1f1c848b
Alex Rael <alprael22@gmail.com>
2018-10-05 14:35:31 -0600

    working code

--- src/edu/cnm/deepdive/Sandbox.java
+++ src/edu/cnm/deepdive/Sandbox.java
@@ -1,19 +1,37 @@
 package edu.cnm.deepdive;
 
+import javax.sound.midi.SysexMessage;
+
 public class Sandbox {
 
-    int num = 50;
-    int x = 20;
-    int y = 9;
-    int z = 6;
-    int nug;
+  public static void main(String[] args) {
+    if (isNug(10)) {
+      System.out.println("is nug");
+    } else System.out.println("not nug");
+  }
 
-    public  boolean isNug() {
-      if ((num % x == 0) || (num % y == 0) || (num % z == 0)) {
-        return true;
-        if () {
 
+    public static boolean isNug (int num){
+      int x = 20;
+      int y = 9;
+      int z = 6;
+      boolean nug = true;
+      boolean notNug = false;
+      while (num >= z) {
+        if ((num % x == 0) || (num % y == 0) || (num % z == 0)) {
+          return nug;
+        } else if (num / x > 1) {
+          num -= x;
+          return isNug(num);
+        } else if (num / y > 1) {
+          num -= y;
+          return isNug(num);
+        } else if (num / z > 1) {
+          num -= z;
+          return isNug(num);
         }
+        break;
       }
+      return notNug;
     }
-}
+  }
